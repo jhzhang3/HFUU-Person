408f6f1c7157ef8c590a49f380a154e5a0ca7e37
jhzhang <2689690138@qq.com>
2024-09-11 23:39:33 +0800

    2024-9-11

--- Leetcode.py
+++ Leetcode.py
@@ -26,10 +26,68 @@ class Solution(object):
         :type target: int
         :rtype: List[int]
         """
-        hashtable = dict()
-        for i, num in enumerate(nums):
-            if target - num in hashtable:
-                return [hashtable[target - num], i]
-            hashtable[nums[i]] = i
-        return []
+        hashtable = dict()  # 哈希表
+        for i, num in enumerate(nums):  # 枚举数组
+            if target - num in hashtable:            # 如果哈希表中存在目标值减去当前值，则找到了
+                return [hashtable[target - num], i]  # 返回下标
+            hashtable[nums[i]] = i  # 哈希表中添加当前值和下标
+        return []         # 如果没有找到，返回空列表
                 
+'''
+2. 移动零
+给定一个数组 nums，编写一个函数将所有 0 移动到数组的末尾，同时保持非零元素的相对顺序。
+请注意 ，必须在不复制数组的情况下原地对数组进行操作。
+示例 1:
+输入: nums = [0,1,0,3,12]
+输出: [1,3,12,0,0]
+示例 2:
+输入: nums = [0]
+输出: [0]
+'''
+
+class Solution(object):
+    def moveZeroes(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: None Do not return anything, modify nums in-place instead.
+        """
+        n = len(nums)
+        left=0
+        right=1
+        while right<n:
+            if nums[right]!=0:
+                nums[left],nums[right]=nums[right],nums[left]
+                left+=1
+            right+=1    
+        return nums
+
+
+'''
+3. 字母异位词分组
+给你一个字符串数组，请你将 字母异位词 组合在一起。可以按任意顺序返回结果列表。
+字母异位词 是由重新排列源单词的所有字母得到的一个新单词。
+
+示例 1:
+
+输入: strs = ["eat", "tea", "tan", "ate", "nat", "bat"]
+输出: [["bat"],["nat","tan"],["ate","eat","tea"]]
+示例 2:
+
+输入: strs = [""]
+输出: [[""]]
+示例 3:
+
+输入: strs = ["a"]
+输出: [["a"]]
+'''
+class Solution(object):
+    def groupAnagrams(self, strs):
+        """
+        :type strs: List[str]
+        :rtype: List[List[str]]
+        """
+        if strs == []:
+            return [[]]
+            
+
+
